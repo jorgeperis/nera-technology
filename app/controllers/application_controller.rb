af7c9b9159cb13cdfc2e8012207bf5a7de5aaf12
Jorge Peris <jorgeperis@gmail.com>
2019-10-19 16:21:09 +0200

    Add environment variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: 'user', password: 'pass'
+  http_basic_authenticate_with name: ENV['NERA_USER'], password: ENV['NERA_PASS']
 
   def index
 
