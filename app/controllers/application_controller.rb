19d57eb84bcbf9e9b2bb444ada4adc4e99ec71c2
Jorge Peris <jorgeperis@gmail.com>
2019-11-17 17:38:31 +0100

    Improve RegExp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
 
           array_data = data["XSerie"]["TEXT"]["s"]
           fecha = array_data[1]
-          measures = array_data.select{ |i| i[/\s\d{1,}\:\s\d{2,}\,\d{2}\s\g/] }
+          measures = array_data.select{ |i| i[/\d{1,}\,\d{2}\s\g/] }
           measures_data = measures.map do |measure|
-            measure[/\d{2,}\,\d{2}/].gsub(',', '.').to_f
+            measure[/\d{1,}\,\d{2}/].gsub(',', '.').to_f
           end
 
           sheet.add_row measures_data.prepend(fecha)
